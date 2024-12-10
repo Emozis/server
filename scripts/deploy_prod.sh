66d0cd5eb36d042784af328176645ef848f0993f
MinkyoDev <isakin.tyrfing@gmail.com>
2024-12-10 23:01:28 +0900

    [Deploy]: permision error 해결

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -9,4 +9,16 @@ VERSION=$4
 # 프로젝트 디렉토리 생성
 mkdir -p $PROJECT_PATH
 
-docker pull "${DOCKER_USERNAME}/${IMAGE_NAME}:${VERSION}"
+sudo docker pull "${DOCKER_USERNAME}/${IMAGE_NAME}:${VERSION}"
+
+docker run -d \
+  --name emogi-app \
+  -v ${PROJECT_PATH}/.env.dev:/app/.env.dev \
+  -v ~/.aws:/root/.aws \
+  -p 8000:8000 \
+  --health-cmd='python -c "import urllib.request; urllib.request.urlopen(\"http://localhost:8000/health\")"' \
+  --health-interval=10s \
+  --health-timeout=5s \
+  --health-retries=5 \
+  --health-start-period=20s \
+  "${DOCKER_USERNAME}/${IMAGE_NAME}:${VERSION}"
