e9065ce0dda19fe572eeb3d5bf59bc4ae84d90f2
MinkyoDev <isakin.tyrfing@gmail.com>
2024-12-10 23:06:38 +0900

    [Deploy]: container stop, rm 명령어 추가

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -5,14 +5,19 @@ PROJECT_PATH=$1
 DOCKER_USERNAME=$2
 IMAGE_NAME=$3
 VERSION=$4
+CONTAINER_NAME=$5
 
 # 프로젝트 디렉토리 생성
 mkdir -p $PROJECT_PATH
 
 sudo docker pull "${DOCKER_USERNAME}/${IMAGE_NAME}:${VERSION}"
 
-docker run -d \
-  --name emogi-app \
+sudo docker stop "${CONTAINER_NAME}" | true
+
+sudo docker rm "${CONTAINER_NAME}" | true
+
+sudo docker run -d \
+  --name "${CONTAINER_NAME}" \
   -v ${PROJECT_PATH}/.env.dev:/app/.env.dev \
   -v ~/.aws:/root/.aws \
   -p 8000:8000 \
