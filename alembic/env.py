4100eff85a7286efb1faecf45e70e957e6f165d5
MinkyoDev <isakin.tyrfing@gmail.com>
2025-01-06 18:56:28 +0900

    [Refactor]: env.py 수정

--- alembic/env.py
+++ alembic/env.py
@@ -8,12 +8,14 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
-# Base만 직접 import
 from app.database.base import Base
 from app.core.settings import settings
 
 # this is the Alembic Config object
 config = context.config
+
+# DB URL 설정 전에 출력해서 확인
+print(f"Database URL: {settings.POSTGRES_HOST}")
 
 # DB URL 설정
 config.set_main_option("sqlalchemy.url", settings.POSTGRES_HOST)
@@ -22,5 +24,44 @@ config.set_main_option("sqlalchemy.url", settings.POSTGRES_HOST)
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
 target_metadata = Base.metadata
+
+def run_migrations_offline() -> None:
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+def do_run_migrations(connection: Connection) -> None:
+    context.configure(connection=connection, target_metadata=target_metadata)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+async def run_async_migrations() -> None:
+    """Run migrations in an async context."""
+    connectable = async_engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode."""
+    asyncio.run(run_async_migrations())
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
