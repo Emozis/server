d6b4a482829846c5db53b46ea289eeb87faa01cc
MinkyoDev <isakin.tyrfing@gmail.com>
2025-01-11 15:41:53 +0900

    [Refactor]: html folder 구조 변경

--- app/api/pages/admin_router.py
+++ app/api/pages/admin_router.py
@@ -7,29 +7,36 @@ router = APIRouter(
     tags=["Pages"]
 )
 
+# Privacy
+@router.get("/privacy")
+async def login_page():
+    return FileResponse("app/static/privacy.html")
+
+# Login
 @router.get("/admin/login")
 async def login_page():
     return FileResponse("app/static/html/login.html")
 
-
+# Dashboard
 @router.get("/admin/dashboard")
 async def login_page():
     return FileResponse("app/static/html/dashboard.html")
 
 # Character
-
 @router.get("/admin/character")
 async def login_page():
-    return FileResponse("app/static/html/characterList.html")
+    return FileResponse("app/static/html/character/characterList.html")
 
 @router.get("/admin/character/detail/{character_id}")
 async def character_detail_page(character_id: int):
-    return FileResponse("app/static/html/characterDetail.html")
+    return FileResponse("app/static/html/character/characterDetail.html")
 
 # Image
 @router.get("/admin/image")
 async def login_page():
-    return FileResponse("app/static/html/imageList.html")
+    return FileResponse("app/static/html/image/imageList.html")
+
+
 # ==========================================
 
 
